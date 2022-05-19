0fe4a6ed594bf6f76ced593b4e3f925ac28cf23a
giakhanh-dev <khanh.lieugia@gmail.com>
2022-05-19 15:18:08 +0700

    [INIT]: Init application, set up server and connect to MongoDB

---
+++ server.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const server: express.Express = express();
+
+server.use(express.json());
+
+server.listen(process.env.PORT || 8545, () => {
+  console.log(`Server is listening at port ${process.env.PORT} !`);
+});
+
+// Trying to connect to MongoDB
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_ATLAS_CONNECTION_STRING || "");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    // Error on initial connection
+    console.log(`Failed to connect to MongoDB, error: ${error}`);
+  }
+};
+
+mongoose.connection.on("connecting", () => {
+  console.log("Connecting to MongoDB...");
+});
+
+// Error after initial connection
+mongoose.connection.on("error", (error: mongoose.CallbackError) => {
+  console.log(`An error has occurred, error: ${error}`);
+});
+
+connectToMongoDB();
